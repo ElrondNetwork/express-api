0b7911ed221972f1d741c360356a2bf3431688a5
Constantin Tovisi <constantin.tovisi@elrond.com>
2021-05-28 16:13:23 +0300

    correct providers for testnet

--- handlers/configs/testnet.js
+++ handlers/configs/testnet.js
@@ -3,7 +3,7 @@ const config = require('./config');
 config.elasticUrls = ['https://testnet-index.elrond.com'];
 config.gatewayUrls = ['https://testnet-gateway.elrond.com'];
 config.apiUrl = 'https://devnet-api.elrond.com';
-config.providersUrl = 'https://devnet-delegation.maiarbrowser.com/providers';
+config.providersUrl = 'https://delegation.maiarbrowser.com/providers';
 config.network = 'testnet';
 
 module.exports = config;
