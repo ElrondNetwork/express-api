c4fdb8887cea5d23ab38e088a1d652899f74d155
pislaProgramming <alex.pislariu2000@gmail.com>
2021-06-18 10:47:11 +0300

    Debug proxy handler

--- handlers/proxy.js
+++ handlers/proxy.js
@@ -11,6 +11,10 @@ exports.handler = async ({ httpMethod, body, path }) => {
   //   throw new Error('move to gateway hostname');
   // }
 
+  console.log('method', httpMethod);
+  console.log('path', path);
+  console.log('body', body);
+
   try {
     const { data } = await axios({
       method: httpMethod.toLowerCase(),
